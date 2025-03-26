aa074ef637d60c687c1dbac6d52a1cb0d603e0a9
elijahpaul27 <alinojal7@gmail.com>
2025-03-26 22:33:08 +0800

    Connected database login credentials

---
+++ daebak-subdivision-website/Models/Admin.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace daebak_subdivision_website.Models
+{
+    [Table("ADMINS")] // Ensure mapping to ADMINS table
+    public class Admin
+    {
+        [Key]
+        [Column("ADMIN_ID")]
+        public int AdminId { get; set; }  // Maps to ADMIN_ID
+
+        [Column("USER_ID")]
+        public int UserId { get; set; }  // Maps to USER_ID
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+    }
+}
---
+++ daebak-subdivision-website/Models/Staff.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace daebak_subdivision_website.Models
+{
+    [Table("STAFF")] // Ensure mapping to STAFF table
+    public class Staff
+    {
+        [Key]
+        [Column("STAFF_ID")]
+        public int StaffId { get; set; }  // Maps to STAFF_ID
+
+        [Column("USER_ID")]
+        public int UserId { get; set; }  // Maps to USER_ID
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+    }
+}
